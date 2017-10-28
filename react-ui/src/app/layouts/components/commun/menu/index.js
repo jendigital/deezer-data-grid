a0e50055245c2abd58223f8adacdb1d5dce5d4ab
Jennifer Lim <contact@jendigitalvision.com>
2017-10-28 23:34:09 +0200

    US-05: hide/show filter

--- react-ui/src/app/layouts/components/commun/menu/index.js
+++ react-ui/src/app/layouts/components/commun/menu/index.js
@@ -6,7 +6,7 @@ class Menu extends Component {
     render() {
         return (
             <div id='header'>
-                <a href='https://github.com/jendigital/deezer-data-grid' target='_blank'>
+                <a href='https://github.com/jendigital/deezer-data-grid' target='_blank' rel="noopener noreferrer">
                     <div id='logo_container'>
                         <img className='logo' alt='Deezer' src='https://storage.googleapis.com/deezer-data-grid/Deezer_logo.png' />
                     </div>
